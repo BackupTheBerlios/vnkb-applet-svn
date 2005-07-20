f37fed643f62837517e83b90d382590a7807f983
pclouds <pclouds@707f00db-b9d6-0310-bb10-f09a9ba66d7d>
2005-07-20 08:42:10 +0000

    ------------------------------------------------------------------------ r7 | pclouds | 2005-07-10 18:40:53 +0700 (CN, 10 Th07 2005) | 4 lines
    Used show_gloss_item in show_gloss_div.
    Added 'View comments' in show_gloss_item.
    Added evgs database structure
    
    ------------------------------------------------------------------------

---
+++ misc/evgs/docs/evgs.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `evgs_glossary_items` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `rev_id` int(10) unsigned NOT NULL default '1',
+  `topic_id` int(10) unsigned NOT NULL default '0',
+  `src` varchar(100) NOT NULL default '',
+  `dst` varchar(100) NOT NULL default '',
+  `user_id` mediumint(10) unsigned NOT NULL default '0',
+  `username` varchar(200) NOT NULL default '',
+  `ctime` int(10) unsigned NOT NULL default '0',
+  `mtime` int(10) unsigned NOT NULL default '0',
+  `description` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM;
+
+CREATE TABLE `evgs_glossrev_items` (
+  `id` int(10) unsigned NOT NULL default '0',
+  `rev_id` int(10) unsigned NOT NULL default '0',
+  `topic_id` int(10) unsigned NOT NULL default '0',
+  `src` varchar(100) NOT NULL default '',
+  `dst` varchar(100) NOT NULL default '',
+  `user_id` mediumint(10) unsigned NOT NULL default '0',
+  `username` varchar(200) NOT NULL default '',
+  `ctime` int(10) unsigned NOT NULL default '0',
+  `mtime` int(10) unsigned NOT NULL default '0',
+  `description` text NOT NULL,
+  PRIMARY KEY  (`id`,`rev_id`)
+) TYPE=MyISAM;
+
+-- INSERT INTO `evgs_config` VALUES ('o_evgs_forum', '1');
--- misc/evgs/upload/include/glossary.php
+++ misc/evgs/upload/include/glossary.php
@@ -9,38 +9,7 @@ function glossary_item_div($topic_id)
 	$cur_gloss = $db->fetch_assoc($result);
 	$cur_gloss['description'] = parse_message($cur_gloss['description'],'1');
 	$username = '<a href="profile.php?id='.$cur_gloss['user_id'].'">'.pun_htmlspecialchars($cur_gloss['username']).'</a>';
-
-?>
-<div class="blockpost">
-	<h2><?php echo '__Glossary';?></h2>
-	<div class="box">
-		<div class="inbox">
-			<div class="postleft">
-				<dl>
-					<dt><strong><?php echo $username ?></strong></dt>
-					<dd class="usertitle"><strong><?php echo $user_title ?></strong></dd>
-					<dd class="postavatar"><?php echo $user_avatar ?></dd>
-<?php if (count($user_info)) echo "\t\t\t\t\t".implode('</dd>'."\n\t\t\t\t\t", $user_info).'</dd>'."\n"; ?>
-<?php if (count($user_contacts)) echo "\t\t\t\t\t".'<dd class="usercontacts">'.implode('&nbsp;&nbsp;', $user_contacts).'</dd>'."\n"; ?>
-				</dl>
-			</div>
-			<div class="postright">
-				<h3><?php echo $cur_gloss['src'].' '.$cur_gloss['dst']; ?></h3>
-				<div class="postmsg">
-					<dl>
-						<dd>__Source: <?php echo pun_htmlspecialchars($cur_gloss['src']); ?></dd>
-						<dd>__Translation: <?php echo pun_htmlspecialchars($cur_gloss['dst']); ?></dd>
-					</dl>
-					<?php echo $cur_gloss['description']; ?>
-				</div>
-			</div>	
-			<div class="clearer"></div>
-			<div class="postfootleft"><?php if ($cur_gloss['user_id'] > 1) echo '<p>'.$is_online.'</p>'; ?></div>
-			<div class="postfootright"><div>&nbsp;</div></div>
-		</div>
-	</div>
-</div>
-<?php
+	show_gloss_item($cur_gloss);
 }
 
 function show_newgloss_box($cur_gloss = null,$edit_mode = false)
@@ -148,6 +117,8 @@ function show_gloss_item($cur_gloss,$history_mode = false)
 <?php if ($cur_gloss['topic_id'] == 0): ?>
 					<li><a href="post.php?evgs=<?php echo $cur_gloss['id'] ?>&fid=<?php echo $pun_config['o_evgs_forum'] ?>">Comment</a></li>
 <?php else: ?>
+					<li><a href="viewtopic.php?id=<?php echo $cur_gloss['topic_id'] ?>">View comments</a></li>
+					<li><?php echo $lang_topic['Link separator'] ?></li>
 					<li><a href="post.php?tid=<?php echo $cur_gloss['topic_id'] ?>">Comment</a></li>
 <?php endif; ?>
 					<li><?php echo $lang_topic['Link separator'] ?></li>
