c47c95371064af5321c4837741f3ae305b55ff2b
pclouds <pclouds@707f00db-b9d6-0310-bb10-f09a9ba66d7d>
2005-07-30 04:55:51 +0000

    Updated to gen php script

--- misc/evgs/extras/evgs2punbb.php
+++ misc/evgs/extras/evgs2punbb.php
@@ -3,12 +3,22 @@ define('PUN_ROOT','./');
 include PUN_ROOT.'include/common.php';
 $result = $db->query("SELECT * FROM evgs_data");
 $now = time();
+/*
 while ($e = $db->fetch_assoc($result)) {
   $sql[] = 'INSERT INTO '.$db->prefix.'glossary_items (src,dst,user_id,username,ctime,mtime,description) VALUES(\''.$db->escape($e['term_en']).'\',\''.$db->escape($e['term_vi']).'\',2,\'admin\','.$now.','.$now.',\''.$db->escape($e['note']).'\')';
 }
-foreach ($sql as $s) {
+*/
+echo "<?php\n";
+?>
+define('PUN_ROOT','./');
+include PUN_ROOT.'include/common.php';
+$now = time();
+<?php
 // Uncomment this line to do the real work
 //  if (!$db->query($s)) echo $db->error();
-  echo "$s<br/>\n";
+while ($e = $db->fetch_assoc($result)) {
+?>
+$db->query('INSERT INTO '.$db->prefix.'glossary_items (src,dst,user_id,username,ctime,mtime,description) VALUES(\''.$db->escape('<?php echo addslashes($e['term_en'])?>').'\',\''.$db->escape('<?php echo addslashes($e['term_vi'])?>').'\',2,\'admin\','$now.','.$now.',\''.$db->escape('<?php echo addslashes($e['note'])?>').'\')');
+<?php
 }
 ?>
