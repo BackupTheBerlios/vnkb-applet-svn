c3845f47aec3e2a8f3c9a64ae42d6e81d5c85e84
pclouds <pclouds@707f00db-b9d6-0310-bb10-f09a9ba66d7d>
2005-07-30 06:19:50 +0000

    Added evgs.php containing initial evgs db

--- misc/evgs/extras/evgs2punbb.php
+++ misc/evgs/extras/evgs2punbb.php
@@ -18,7 +18,15 @@ $now = time();
 //  if (!$db->query($s)) echo $db->error();
 while ($e = $db->fetch_assoc($result)) {
 ?>
-$db->query('INSERT INTO '.$db->prefix.'glossary_items (src,dst,user_id,username,ctime,mtime,description) VALUES(\''.$db->escape('<?php echo addslashes($e['term_en'])?>').'\',\''.$db->escape('<?php echo addslashes($e['term_vi'])?>').'\',2,\'admin\','$now.','.$now.',\''.$db->escape('<?php echo addslashes($e['note'])?>').'\')');
+$db->query('INSERT INTO '.$db->prefix.'glossary_items (src,dst,user_id,username,ctime,mtime,description) VALUES(\''.$db->escape('<?php echo str2php($e['term_en'])?>').'\',\''.$db->escape('<?php echo str2php($e['term_vi'])?>').'\',2,\'admin\','$now.','.$now.',\''.$db->escape('<?php echo str2php($e['note'])?>').'\')');
 <?php
+}
+function str2php($str)
+{
+	return addslashes($str);
+	$str = str_replace("'","\\'",$str);
+	$str = str_replace("\n","\\n",$str);
+	$str = str_replace("\r","\\r",$str);
+	return $str;
 }
 ?>
