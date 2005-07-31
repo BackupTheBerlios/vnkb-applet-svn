6750187750aabfc07b8ba666a698002f027e1a80
pclouds <pclouds@707f00db-b9d6-0310-bb10-f09a9ba66d7d>
2005-07-31 08:05:16 +0000

    Limit the result

--- misc/evgs/upload/glossajax.php
+++ misc/evgs/upload/glossajax.php
@@ -3,7 +3,8 @@ define ('PUN_ROOT','./');
 define('PUN_QUIET_VISIT', 1);
 include PUN_ROOT.'include/common.php';
 $pattern = $_GET['id'];
-$result = $db->query('SELECT DISTINCT src FROM '.$db->prefix.'glossary_items WHERE src LIKE \''.$db->escape($pattern).'%\' ORDER BY src');
+$max = isset($_GET['max']) ? ' LIMIT '.intval($_GET['max']) : '';
+$result = $db->query('SELECT DISTINCT src FROM '.$db->prefix.'glossary_items WHERE src LIKE \''.$db->escape($pattern).'%\' ORDER BY src '.$max);
 $first_row = true;
 while ($row = $db->fetch_row($result)) {
 	if ($first_row)
